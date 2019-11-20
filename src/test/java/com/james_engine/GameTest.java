c6add494dfe87bcf6302d4897afff1816387994f
Wayne A. Tonnesen <wayne.tonnesen@gmail.com>
2019-11-19 20:37:00 -0800

    Replaced default app with Game class

---
+++ src/test/java/com/james_engine/GameTest.java
@@ -0,0 +1,14 @@
+package com.james_engine;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class GameTest {
+
+    @Test
+    public void thisShouldPass() {
+        assertFalse(false);
+    }
+
+}
